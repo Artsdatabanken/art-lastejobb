b5b750c785fd3e906de2dc622c48111ae19b838c
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-09-30 01:01:58 +0200

    Remove type

--- stages/transform/80/type.js
+++
@@ -1,7 +0,0 @@
-const { io } = require("lastejobb");
-
-const toplevel = io.lesDatafil("art-takson/type").items;
-const fatop = io.lesDatafil("fremmed-art/type").items;
-
-fatop.forEach(e => toplevel.push(e));
-io.skrivBuildfil(__filename, toplevel);
