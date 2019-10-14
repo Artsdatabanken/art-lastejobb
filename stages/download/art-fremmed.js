e889883758a0a0dcfe032b0e06f71a7f72c59601
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-10-14 12:29:28 +0200

    fremmed-art -> art-fremmed

--- stages/download/art-fremmed.js
+++ stages/download/art-fremmed.js
@@ -1,6 +1,6 @@
 const { git } = require("lastejobb");
 
 git.clone(
-  "https://github.com/Artsdatabanken/fremmed-art.git",
+  "https://github.com/Artsdatabanken/art-fremmed.git",
   "data/art-fremmed"
 );
